e73f1086b46a8b68c99bb3eaeb6bc34b98065a24
Gino Carlucci <ginocarlucci@hotmail.com>
2020-09-04 11:38:22 -0300

    termino lab 02

--- lab02.sql
+++ lab02.sql
@@ -61,8 +61,57 @@ group by ve.`dni`;
 select *
 from personas p
 where p.`fecha_tentativa`>'2007-11-24'
-AND p.dni is null
+AND p.dni is null;
 
 #SQL8
+select e.dni,e.`apellidoynombres`,count(*) cant_serv
+from empleados e
+     inner join `contratos_por_eventos` c
+           on e.`dni`=c.`dni_empleado`
+     inner join `servicios_contratos` sc
+           on c.`nro_contrato`=sc.`nro_contrato`
+group by e.dni,e.`apellidoynombres`
+order by cant_serv;
+
+#SQL9
+select p.`dni`,p.`apellidoynombre`,e.`dni`, e.`apellidoynombres`,count(*) cant_eventos
+from personas p
+     inner join `contratos_por_eventos` c
+           on p.`idpersona`=c.`idpersona`
+     inner join empleados e
+           on e.`dni`=c.`dni_empleado`
+group by p.`dni`,p.`apellidoynombre`,e.`dni`, e.`apellidoynombres`;
+
+#SQL10
+select p.apellidoynombre, cpe.nro_contrato, cpe.fecha_evento, count(*) cantidad_servicios
+from personas p
+inner join contratos_por_eventos cpe on p.idpersona = cpe.idpersona
+inner join servicios_contratos sc on cpe.nro_contrato = sc.nro_contrato
+group by p.apellidoynombre, cpe.nro_contrato, cpe.fecha_evento
+having cantidad_servicios <= 3;
+
+#SQL11
+select p.*
+from personas p
+     inner join `contratos_por_eventos` c
+           on p.`idpersona`=c.`idpersona`
+where c.`salon_ubicacion` is null;
+
+#SQL12
+select *
+from `personas`
+where personas.`idpersona` not in (select p.`idpersona`
+                                   from personas p
+                                        inner join `contratos_por_eventos` c
+                                                   on p.`idpersona`=c.`idpersona`
+                                   where c.`salon_ubicacion` is null);
+#SQL13
+update `valores_empleados`
+set sueldo_basico=sueldo_basico*1.07
+where sueldo_basico>=1000 and anio_valor=2007 and mes_valor=10;
+
+update `valores_empleados`
+set sueldo_basico=sueldo_basico*1.1
+where sueldo_basico<1000 and anio_valor=2007 and mes_valor=10;
 
 
